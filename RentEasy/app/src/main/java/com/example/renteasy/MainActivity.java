1624f6845a343d12c74a3034e49bc0ff2b52519d
Raj <ketakishah369@gmail.com>
2023-08-01 12:04:21 +0530

    Login UI Responsive UI AND Custom Style Creation

--- RentEasy/app/src/main/java/com/example/renteasy/MainActivity.java
+++ RentEasy/app/src/main/java/com/example/renteasy/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.renteasy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button Login = findViewById(R.id.Login);
+        Login.setOnClickListener(view -> {
+            System.out.println("Logged In Successfully");
+            Toast.makeText(MainActivity.this,"Logged In Successfully",Toast.LENGTH_SHORT).show();
+            Snackbar.make(view,"Successfully Logged In", Snackbar.LENGTH_SHORT).show();
+        });
+
     }
 }
